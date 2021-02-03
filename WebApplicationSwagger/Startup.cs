86af60a4969bb957c1d703b490c19cd8640a942c
luzhaoguang <2308921670@qq.com>
2021-02-03 23:16:13 +0800

    1

--- WebApplicationSwagger/Startup.cs
+++ WebApplicationSwagger/Startup.cs
@@ -18,6 +18,7 @@ namespace WebApplicationSwagger
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
